c8d86c136524ecee955933347532b597288764a2
Sreeprasad <sreeprasad.sp@gmail.com>
2012-03-19 12:34:31 -0400

    updated code to handle redirects
    Code now redirects to required URL to scrap data.

--- newRa/src/Authenticate.java
+++ newRa/src/Authenticate.java
@@ -1,91 +1,83 @@
 
-	/* CODE: SREEPRASAD */
-
-	/* This code is used to authenticate to website using java */
-
-	/* import functions */
-	import java.io.*;
+/* CODE written by Sreeprasad */
+	
+	/* import statements  */
+	import java.io.*;	 
 	import java.util.ArrayList;
 	import java.util.List;
-
-	import org.apache.http.Header;
 	import org.apache.http.HttpRequest;
 	import org.apache.http.HttpResponse;
 	import org.apache.http.NameValuePair;
 	import org.apache.http.client.HttpClient;
 	import org.apache.http.client.entity.UrlEncodedFormEntity;
 	import org.apache.http.client.methods.HttpPost;
-	import org.apache.http.impl.client.DefaultHttpClient; 
+	import org.apache.http.impl.client.DefaultHttpClient;
 	import org.apache.http.impl.client.DefaultRedirectStrategy;
 	import org.apache.http.message.BasicNameValuePair;
 	import org.apache.http.protocol.HttpContext;
-	import org.apache.http.protocol.BasicHttpContext;
 
-
-
-	public class Authenticate {
-
-		public static void main(String[] args) {
-
+	/* class begins */	
+public class Authenticate {
+	/* main method */	
+    public static void main(String[] args){
+ 	try { 
+		/* initialize client */	
+ 		DefaultHttpClient client = new DefaultHttpClient();
+		/* initialize URL to navigate for authencation for client */	
+		HttpPost post = new HttpPost("REPLACE_WITH_AUTHENTICATION_URL");
 			try {
-				/* get client */ 
-				DefaultHttpClient client = new DefaultHttpClient();
-
-				/* url to logg in */
-				HttpPost post = new HttpPost("https://albert.rit.edu/patroninfo~S3/1147900/top");
-				try {
-					/* get the user name and password to store in list */
-					List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
-					/* adding username */
-					nameValuePairs
-							.add(new BasicNameValuePair("name", "REPLACE_WITH_NAME"));
-					/* adding password */
-					nameValuePairs.add(new BasicNameValuePair("code",
-							"REPLACE_WITH_CODE"));
-					nameValuePairs.add(new BasicNameValuePair("pin",
-							"REPLACE_WITH_PIN"));
-					post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-					/* execute */
-
-
-	 				HttpResponse response = client.execute(post);
-
-					/* added code to handle redirection */
-					
-					
-	 				client.setRedirectStrategy( new DefaultRedirectStrategy(){
-	 				/* The parameter of the function :HttpContext con  throws NoSuchMethod Runtime exception.
-						Currently code is has yet to handle this. I have put up request in stackoverflow
-						http://stackoverflow.com/questions/9706781/basichttpcontext-error-message-on-redirection
-						Till now no one has answered this question. Looking for other options */
-						public boolean isRedirected(HttpRequest req, HttpResponse res, HttpContext con){
-	 						if(res.getStatusLine().getStatusCode()==302) return true;
-	 						return false;
-	 					}
-	 				});
-
-					System.out.println("response is :");
-					System.out.println(response.toString());
-
-
-
-
-
-	 				/* get content */
-					BufferedReader rd = new BufferedReader(new InputStreamReader(
-							response.getEntity().getContent()));
-					/* String to store the output */
-					String line = "";
-					/* Output until end of line */
-					while ((line = rd.readLine()) != null)
-						System.out.println(line);
-				} catch (IOException e) {
-					e.printStackTrace();
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+		/* replace with authentication username and id */	
+				nameValuePairs.add(new BasicNameValuePair("REPLACE_WITH_ID_KEY",
+				                    "REPLACE_WITH_ID_VALUE"));
+		/* replace with authentication password  */	
+				nameValuePairs.add(new BasicNameValuePair("REPLACE_WITH_PASSWORD_KEY",
+				                    "REPLACE_WITH_PASSWORD_VALUE"));
+				post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+				
+				System.out.println("response is :");
+				/* redirection to navigate after redirection */
+				client.setRedirectStrategy( new DefaultRedirectStrategy(){
+ 					public boolean isRedirected(HttpRequest req, HttpResponse res, HttpContext con){
+ 						if(res.getStatusLine().getStatusCode()==302) return true;
+ 						return false;
+ 					}
+ 				});
+				/* get reponse from server */
+				HttpResponse response = client.execute(post); 
+				BufferedReader rd = new BufferedReader(new InputStreamReader(
+						response.getEntity().getContent()));
+				String line = "";
+				/* this is not required. Just added to provide layer of debugging. */
+				while ((line = rd.readLine()) != null) {
+					//System.out.println(line);
 				}
-			} catch (Exception e) {
-				System.out.println("Could not logg in..");
-				System.out.println(e);
-				e.printStackTrace();
-			}
-		}
+				/* navigate to the url to scrap data */				
+				HttpPost new_post = new HttpPost("REPLACE_WITH_NAVIGATION_URL");
+				/* get response from url to scrap data */
+				HttpResponse new_response = client.execute(new_post); 
+				
+				BufferedReader new_rd = new BufferedReader(new InputStreamReader(
+						new_response.getEntity().getContent()));
+				String new_line = "";
+				while ((new_line = new_rd.readLine()) != null) {
+					/* render reponse before scrapping data */
+					System.out.println(new_line);
+				}
+				
+			       /* catch Exceptions */
+				} catch (IOException e) {
+						e.printStackTrace();
+				}
+		} 
+		catch(Exception e) 
+		{ 
+			System.out.println( "Exception caught while navigating." ); 
+			System.out.println( e.toString() ); 
+			e.printStackTrace(); 
+		} 
 	}
+}
+	
+	
+
